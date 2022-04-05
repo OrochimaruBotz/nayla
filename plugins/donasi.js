ffb2607306a0b81b959ea1bd99a42e277e64d826
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-04-05 14:45:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
 ┏━━°❀❬ *DONASI* ❭❀°━━┓
-┣➥ *Smartfren:* [08818770766]
-┣➥ *Tri:* [089520306297]
-┣➥ *Dana:* [08818770766]
-┣➥ *Saweria:* [https://saweria.co/Scaff]
+┣➥ *Smartfren:* [-]
+┣➥ *Tri:* [-]
+┣➥ *Dana:* [-]
+┣➥ *Saweria:* [-]
 ┃ 「 *Chat OWNER* 」
-┃ > *Ingin donasi? Wa.me/628818770766*
+┃ > *Ingin donasi? Wa.me/6283131214121*
 ┗━━━━━━━━━━━━━━━━
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
