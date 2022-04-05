d1518f8ee09bc2292597def6c407e88f7d244345
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-04-05 15:04:41 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '628818770766', owname, m)
-  conn.sendContact(m.chat, '628818770766', owname, m)
-  conn.reply(m.chat, `Tuh Ownerku hehe`, m)
+  // this.sendContact(m.chat, '6283131214121', owname, m)
+  conn.sendContact(m.chat, '6283131214121', owname, m)
+  conn.reply(m.chat, `Tuh Ownerku kak cuman save wa cewe ya kak`, m)
 }
 handler.help = ['owner']
 handler.tags = ['info']
